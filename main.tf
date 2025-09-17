63e6f5bf2b5f525d00f461b5e3f040ecd75bdae2
Rafael Schneider <rafaelscone@gmail.com>
2025-09-17 19:42:06 +0100

    fix

--- main.tf
+++ main.tf
@@ -9,13 +9,6 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region     = var.awsRegion
-  #access_key = var.access_key
-  #secret_key = var.secret_key
-  #   profile = "sagar"
-}
-
 resource "aws_instance" "example_server" {
   ami           = "ami-0ff8a91507f77f867"
   instance_type = var.instanceType
